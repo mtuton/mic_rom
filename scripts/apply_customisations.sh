7c5545d85c0d5b4f7d7d618bb698c5f62e3761a7
Mic Tuton <mic@tuton.org>
2011-05-22 16:22:38 +1000

    script: added additional checks before removing files

--- scripts/apply_customisations.sh
+++ scripts/apply_customisations.sh
@@ -2,6 +2,16 @@
 
 # Applies customisation to a ROM
 
+remove_file()
+{
+	file=$1
+	if test -f "$file"; then
+		rm "$file"
+	else
+		echo "$file already removed."
+	fi
+}
+
 echo -n "Copying updater-script..."
 cp -rp customisations/META-INF rom
 echo "done."
@@ -53,45 +63,45 @@ echo "DONE - APPLYING CUSTOM APPS"
 echo "TRIMMING THE ROM"
 
 echo -n "Removing Swype..."
-rm rom/system/app/Swype.apk
-rm rom/system/lib/libSwypeCode.so
+remove_file rom/system/app/Swype.apk
+remove_file rom/system/lib/libSwypeCode.so
 echo "done."
 
 echo -n "Removing Protips..."
-rm rom/system/app/Protips.apk
+remove_file rom/system/app/Protips.apk
 echo "done."
 
 echo -n "Removing Samsung All..." 
-rm rom/system/app/Dlna.apk
+remove_file rom/system/app/Dlna.apk
 echo "done."
 
 echo -n "Removing Sim Toolkit..."
-rm rom/system/app/Stk.apk
+remove_file rom/system/app/Stk.apk
 echo "done."
 
 echo -n "Removing PhoneSetupWizard..."
-rm rom/system/app/PhoneSetupWizard.apk
+remove_file rom/system/app/PhoneSetupWizard.apk
 echo "done."
 
 echo -n "Removing PressReader..."
-rm rom/system/app/PressReader.apk
+remove_file rom/system/app/PressReader.apk
 echo "done."
 
 echo -n "Removing GoogleFeedback..."
-rm rom/system/app/GoogleFeedback.apk
+remove_file rom/system/app/GoogleFeedback.apk
 echo "done."
 
 echo -n "Removing autorun.iso.."
-rm rom/system/etc/autorun.iso
+remove_file rom/system/etc/autorun.iso
 echo "done."
 
 echo -n "Removing startup sounds..."
-rm rom/system/etc/PowerOn.wav
-rm rom/system/etc/PowerOn.snd
+remove_file rom/system/etc/PowerOn.wav
+remove_file rom/system/etc/PowerOn.snd
 echo "done."
 
 echo -n "Removing battery notification tone..."
-rm rom/system/media/audio/ui/TW_Battery_caution.ogg
+remove_file rom/system/media/audio/ui/TW_Battery_caution.ogg
 echo "done."
 
 echo "DONE - TRIMMING THE ROM"
@@ -100,58 +110,58 @@ echo "DONE - TRIMMING THE ROM"
 # All these files will be created as symlinks when the ROM is installed
 
 echo -n "Cleaning up ROM..."
-rm rom/system/zipalign.log
-rm rom/system/bin/cat
-rm rom/system/bin/chmod
-rm rom/system/bin/chown
-rm rom/system/bin/cmp
-rm rom/system/bin/date
-rm rom/system/bin/dd
-rm rom/system/bin/df
-rm rom/system/bin/dmesg
-rm rom/system/bin/getevent
-rm rom/system/bin/getprop
-rm rom/system/bin/hd
-rm rom/system/bin/id
-rm rom/system/bin/ifconfig
-rm rom/system/bin/iftop
-rm rom/system/bin/insmod
-rm rom/system/bin/ioctl
-rm rom/system/bin/ionice
-rm rom/system/bin/kill
-rm rom/system/bin/ln
-rm rom/system/bin/log
-rm rom/system/bin/ls
-rm rom/system/bin/lsmod
-rm rom/system/bin/mkdir
-rm rom/system/bin/mount
-rm rom/system/bin/mv
-rm rom/system/bin/nandread
-rm rom/system/bin/netstat
-rm rom/system/bin/newfs_msdos
-rm rom/system/bin/notify
-rm rom/system/bin/printenv
-rm rom/system/bin/ps
-rm rom/system/bin/reboot
-rm rom/system/bin/renice
-rm rom/system/bin/rm
-rm rom/system/bin/rmdir
-rm rom/system/bin/rmmod
-rm rom/system/bin/route
-rm rom/system/bin/schedtop
-rm rom/system/bin/sendevent
-rm rom/system/bin/setconsole
-rm rom/system/bin/setprop
-rm rom/system/bin/sleep
-rm rom/system/bin/smd
-rm rom/system/bin/start
-rm rom/system/bin/stop
-rm rom/system/bin/sync
-rm rom/system/bin/top
-rm rom/system/bin/umount
-rm rom/system/bin/vmstat
-rm rom/system/bin/watchprops
-rm rom/system/bin/wipe
-rm rom/system/bin/dumpmesg
-rm rom/system/bin/csview
+remove_file rom/system/zipalign.log
+remove_file rom/system/bin/cat
+remove_file rom/system/bin/chmod
+remove_file rom/system/bin/chown
+remove_file rom/system/bin/cmp
+remove_file rom/system/bin/date
+remove_file rom/system/bin/dd
+remove_file rom/system/bin/df
+remove_file rom/system/bin/dmesg
+remove_file rom/system/bin/getevent
+remove_file rom/system/bin/getprop
+remove_file rom/system/bin/hd
+remove_file rom/system/bin/id
+remove_file rom/system/bin/ifconfig
+remove_file rom/system/bin/iftop
+remove_file rom/system/bin/insmod
+remove_file rom/system/bin/ioctl
+remove_file rom/system/bin/ionice
+remove_file rom/system/bin/kill
+remove_file rom/system/bin/ln
+remove_file rom/system/bin/log
+remove_file rom/system/bin/ls
+remove_file rom/system/bin/lsmod
+remove_file rom/system/bin/mkdir
+remove_file rom/system/bin/mount
+remove_file rom/system/bin/mv
+remove_file rom/system/bin/nandread
+remove_file rom/system/bin/netstat
+remove_file rom/system/bin/newfs_msdos
+remove_file rom/system/bin/notify
+remove_file rom/system/bin/printenv
+remove_file rom/system/bin/ps
+remove_file rom/system/bin/reboot
+remove_file rom/system/bin/renice
+remove_file rom/system/bin/rm
+remove_file rom/system/bin/rmdir
+remove_file rom/system/bin/rmmod
+remove_file rom/system/bin/route
+remove_file rom/system/bin/schedtop
+remove_file rom/system/bin/sendevent
+remove_file rom/system/bin/setconsole
+remove_file rom/system/bin/setprop
+remove_file rom/system/bin/sleep
+remove_file rom/system/bin/smd
+remove_file rom/system/bin/start
+remove_file rom/system/bin/stop
+remove_file rom/system/bin/sync
+remove_file rom/system/bin/top
+remove_file rom/system/bin/umount
+remove_file rom/system/bin/vmstat
+remove_file rom/system/bin/watchprops
+remove_file rom/system/bin/wipe
+remove_file rom/system/bin/dumpmesg
+remove_file rom/system/bin/csview
 echo "done."
